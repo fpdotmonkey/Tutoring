02fdd02551b692eaa19f67f4fd1d14f65cccfeff
Fletcher Porter <me@fletcherporter.com>
2020-08-18 18:50:14 -0700

    added a lecture document on C and pointers

---
+++ pass_by_pointer/README.md
@@ -0,0 +1,16 @@
+# C and Pointers
+
+You can read the lecture notes [here](c_and_pointers.md)
+
+To run the code from the lecture, download the source file [pass_by_pointer.c](pass_by_pointer.c) and the [Makefile](Makefile) to their own directory, and follow these instructions.
+
+Linux/macOS
+```bash
+make
+./pass_by_pointer
+# There are 2 apples for each person with 3 left over
+```
+
+Windows
+```
+```
---
+++ pass_by_pointer/pass_by_pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void quotient_and_remainder(int dividend, int divisor,
+                            int *quotient, int *remainder) {
+    *quotient = dividend / divisor;
+    *remainder = dividend % divisor;
+}
+
+void print_apples_per_person(int number_of_apples, int number_of_people) {
+    int apples_per_person = 0;
+    int apples_left_over = 0;
+
+    quotient_and_remainder(number_of_apples, number_of_people,
+                           &apples_per_person, &apples_left_over);
+
+    printf("There are %i apples for each person with %i left over\n",
+           apples_per_person,
+           apples_left_over);
+}
+
+int main(int argc, char **argv) {
+    int number_of_apples = 11;
+    int number_of_people = 4;
+
+    print_apples_per_person(number_of_apples, number_of_people);
+}
